0d73c08fdd4e6349090eec7c8d105400847d7875
JaeHa Song <kiss23v@naver.com>
2019-06-10 22:46:02 +0900

    modified Notice By SJH 22 : 45

--- src/kh/semi/servlets/CategoryController.java
+++ src/kh/semi/servlets/CategoryController.java
@@ -131,7 +131,6 @@ public class CategoryController extends HttpServlet {
 						System.out.println("addr1:"+addr1);		
 
 
-<<<<<<< HEAD
 						if(addr2 == null) {
 							list = dao.getInfoByLocation1(select, addr1, start, end);
 							recordTotalCount = dao.getTotalByMenu("info_addr2", addr1);
@@ -140,25 +139,6 @@ public class CategoryController extends HttpServlet {
 							recordTotalCount = dao.getTotalByMenu2("info_addr2", addr1, "info_addr2", addr2);
 						}		
 					}
-=======
-					if(addr2 == null) {
-						if(addr1.equals("경상도")) {
-							addr1 ="^경북|^경남|^대구|^부산";
-						}else if(addr1.equals("충청도")) {
-							addr1="^충청|^대전";
-						}else if(addr1.equals("전라도")) {
-							addr1="^전라|^광주";
-						}
-						System.out.println(addr1);
-						addr1 = "^"+addr1;
-						list = dao.getInfoByLocation1(select, addr1, start, end);
-						recordTotalCount = dao.getTotalByMenu("info_addr2", addr1);
-					}else {
-						list = dao.getInfoByLocation2(select, addr1, addr2, start, end);
-						recordTotalCount = dao.getTotalByMenu2("info_addr2", addr1, "info_addr2", addr2);
-					}		
-				}
->>>>>>> 006015949d02d54688b1d65cdfee440c47fbb15e
 
 					request.setAttribute("list", list);	
 
